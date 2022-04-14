cc5391e700e4c308f74ff5ea88ded7c1eadb1fba
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-04-14 03:10:06 +0000

    This is my portfolio : first project using django

--- Nassima/views.py
+++ Nassima/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import Home, About, Profile, Category, Skills, Portfolio
 
 # Create your views here.
+
+def index(request):
+    # Home
+    home = Home.objects.latest('updated')
+
+    # About
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+
+    # Skills
+    categories = Category.objects.all()
+
+    # Portfolio
+    portfolios = Portfolio.objects.all()
+    
+
+    context = {
+        'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portfolios': portfolios
+    }
+
+
+    return render(request, 'index.html', context)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +116,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
